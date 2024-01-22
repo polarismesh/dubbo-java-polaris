e6e9c8fc36b129856697792897f0c8484f8cdc10
chuntaojun <liaochuntao@live.com>
2024-01-22 11:53:09 +0800

    deploy:add demo deploy

--- dubbo/dubbo-examples/dubbo-discovery-example/dubbo-quick-provider/src/main/java/com/tencent/polaris/dubbo/discovery/example/provider/Main.java
+++ dubbo/dubbo-examples/dubbo-discovery-example/dubbo-quick-provider/src/main/java/com/tencent/polaris/dubbo/discovery/example/provider/Main.java
@@ -17,20 +17,42 @@
 
 package com.tencent.polaris.dubbo.discovery.example.provider;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
+
+    private static final int LISTEN_PORT = 15700;
+
+    private static final String PATH = "/reload";
+
     public static void main(String[] args) throws Exception {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConsumerConfiguration.class);
-        context.start();
-        System.out.println("dubbo service started");
-        new CountDownLatch(1).await();
+        HttpServer server = HttpServer.create(new InetSocketAddress(LISTEN_PORT), 0);
+        server.createContext(PATH, new EchoClientHandler());
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            server.stop(1);
+        }));
+        server.start();
     }
 
     @Configuration
@@ -40,4 +62,43 @@ public class Main {
     static class ConsumerConfiguration {
 
     }
+
+    private static class EchoClientHandler implements HttpHandler {
+
+        private final Object mutex = new Object();
+
+        private AnnotationConfigApplicationContext context;
+
+        public EchoClientHandler() {
+            int delay = 0;
+            if (!Objects.equals(System.getenv("DELAY_REGISTER"), "")) {
+                delay = Integer.parseInt(System.getenv("DELAY_REGISTER"));
+            }
+            synchronized (mutex) {
+                try {
+                    TimeUnit.MINUTES.sleep(delay);
+                    context = new AnnotationConfigApplicationContext(ConsumerConfiguration.class);
+                    context.start();
+                } catch (InterruptedException ignore) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            synchronized (mutex) {
+               try {
+                   context.destroy();
+                   TimeUnit.MINUTES.sleep(1);
+                   context = new AnnotationConfigApplicationContext(ConsumerConfiguration.class);
+                   context.start();
+               } catch (Exception e) {
+                   e.printStackTrace();
+               }
+            }
+        }
+
+    }
+
 }
