899348754c5c2bbdc653f8de92fdd6baac4018db
chuntaojun <liaochuntao@live.com>
2024-01-22 18:29:22 +0800

    deploy:添加kubernetes的部署

--- dubbo/dubbo-examples/dubbo-discovery-example/dubbo-quick-provider/src/main/java/com/tencent/polaris/dubbo/discovery/example/provider/AnnotatedGreetingService.java
+++ dubbo/dubbo-examples/dubbo-discovery-example/dubbo-quick-provider/src/main/java/com/tencent/polaris/dubbo/discovery/example/provider/AnnotatedGreetingService.java
@@ -24,12 +24,12 @@ import org.apache.dubbo.config.annotation.DubboService;
 public class AnnotatedGreetingService implements GreetingService {
 
     public String sayHello(String name) {
-        return "hello, " + name;
+        return "hello, " + name + ", source from " + System.getenv("POD_IP");
     }
 
     @Override
     public String sayHi(String name) {
-        return "[provider by polaris] hi, " + name;
+        return "[provider by polaris] hi, " + name + ", source from " + System.getenv("POD_IP");
     }
 
 }
