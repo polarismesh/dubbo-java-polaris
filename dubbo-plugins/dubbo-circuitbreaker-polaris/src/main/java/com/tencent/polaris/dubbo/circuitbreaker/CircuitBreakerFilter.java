01462eae7f9f99bfdbd70f8afc51d78323741680
Haotian Zhang <928016560@qq.com>
2025-03-28 17:59:52 +0800

    docs:update license.

---
+++ dubbo-plugins/dubbo-circuitbreaker-polaris/src/main/java/com/tencent/polaris/dubbo/circuitbreaker/CircuitBreakerFilter.java
@@ -0,0 +1,181 @@
+/*
+ * Tencent is pleased to support the open source community by making dubbo-polaris-java available.
+ *
+ * Copyright (C) 2021 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.tencent.polaris.dubbo.circuitbreaker;
+
+import com.tencent.polaris.api.plugin.circuitbreaker.ResourceStat;
+import com.tencent.polaris.api.plugin.circuitbreaker.entity.InstanceResource;
+import com.tencent.polaris.api.plugin.circuitbreaker.entity.Resource;
+import com.tencent.polaris.api.pojo.CircuitBreakerStatus;
+import com.tencent.polaris.api.pojo.RetStatus;
+import com.tencent.polaris.api.pojo.ServiceKey;
+import com.tencent.polaris.api.utils.StringUtils;
+import com.tencent.polaris.circuitbreak.api.CircuitBreakAPI;
+import com.tencent.polaris.circuitbreak.api.InvokeHandler;
+import com.tencent.polaris.circuitbreak.api.pojo.InvokeContext;
+import com.tencent.polaris.circuitbreak.api.pojo.ResultToErrorCode;
+import com.tencent.polaris.circuitbreak.client.exception.CallAbortedException;
+import com.tencent.polaris.common.exception.PolarisBlockException;
+import com.tencent.polaris.common.registry.DubboServiceInfo;
+import com.tencent.polaris.common.registry.PolarisOperator;
+import com.tencent.polaris.common.registry.PolarisOperatorDelegate;
+import com.tencent.polaris.common.utils.DubboUtils;
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.constants.CommonConstants;
+import org.apache.dubbo.common.extension.Activate;
+import org.apache.dubbo.common.logger.ErrorTypeAwareLogger;
+import org.apache.dubbo.common.logger.LoggerFactory;
+import org.apache.dubbo.rpc.*;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.ServiceLoader;
+import java.util.concurrent.TimeUnit;
+
+@Activate(group = CommonConstants.CONSUMER)
+public class CircuitBreakerFilter extends PolarisOperatorDelegate implements Filter, ResultToErrorCode {
+
+    protected final ErrorTypeAwareLogger logger = LoggerFactory.getErrorTypeAwareLogger(getClass());
+
+    private final PolarisOperator operator;
+
+    private final CircuitBreakAPI circuitBreakAPI;
+
+    private CircuitBreakerCallback circuitBreakerCallback;
+
+    public CircuitBreakerFilter() {
+        logger.info("[POLARIS] init polaris circuitbreaker");
+        this.operator = getGovernancePolarisOperator();
+        this.circuitBreakAPI = operator.getCircuitBreakAPI();
+        ServiceLoader<CircuitBreakerCallback> loader = ServiceLoader.load(CircuitBreakerCallback.class);
+        if (loader.iterator().hasNext()) {
+            circuitBreakerCallback = loader.iterator().next();
+        }
+    }
+
+    @Override
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        List<DubboServiceInfo> serviceInfos = DubboUtils.analyzeRemoteDubboServiceInfo(invoker, invocation);
+        if (serviceInfos.isEmpty() || Objects.isNull(operator)) {
+            return invoker.invoke(invocation);
+        }
+        // 如果是熔断，只选择第一个 DubboServiceInfo 进行作为熔断信息
+        DubboServiceInfo firstService = serviceInfos.get(0);
+
+        InvokeContext.RequestContext context = new InvokeContext.RequestContext(createCalleeService(firstService), "dubbo", "dubbo", firstService.getDubboInterface());
+        context.setSourceService(new ServiceKey());
+        context.setResultToErrorCode(this);
+        InvokeHandler handler = circuitBreakAPI.makeInvokeHandler(context);
+        try {
+            long startTimeMilli = System.currentTimeMillis();
+            InvokeContext.ResponseContext responseContext = new InvokeContext.ResponseContext();
+            responseContext.setDurationUnit(TimeUnit.MILLISECONDS);
+            Result result = null;
+            RpcException exception = null;
+            handler.acquirePermission();
+            try {
+                result = invoker.invoke(invocation);
+                responseContext.setDuration(System.currentTimeMillis() - startTimeMilli);
+                if (result.hasException()) {
+                    responseContext.setError(result.getException());
+                    handler.onError(responseContext);
+                } else {
+                    responseContext.setResult(result);
+                    handler.onSuccess(responseContext);
+                }
+            } catch (RpcException e) {
+                exception = e;
+                responseContext.setError(e);
+                responseContext.setDuration(System.currentTimeMillis() - startTimeMilli);
+                handler.onError(responseContext);
+            }
+            ResourceStat resourceStat = createInstanceResourceStat(invoker, invocation, firstService, responseContext, responseContext.getDuration());
+            circuitBreakAPI.report(resourceStat);
+            if (result != null) {
+                return result;
+            }
+            throw exception;
+        } catch (CallAbortedException abortedException) {
+            CircuitBreakerStatus.FallbackInfo fallbackInfo = abortedException.getFallbackInfo();
+            if (Objects.nonNull(fallbackInfo)) {
+                AppResponse response = new AppResponse();
+                response.setAttachments(fallbackInfo.getHeaders());
+                response.setValue(fallbackInfo.getBody());
+                return AsyncRpcResult.newDefaultAsyncResult(response, invocation);
+            }
+            if (Objects.nonNull(circuitBreakerCallback)) {
+                return circuitBreakerCallback.onCircuitBreaker(invoker, invocation, abortedException);
+            }
+            throw new RpcException(abortedException);
+        }
+    }
+
+    private ResourceStat createInstanceResourceStat(Invoker<?> invoker, Invocation invocation, DubboServiceInfo serviceInfo,
+                                                    InvokeContext.ResponseContext context, long delay) {
+        URL url = invoker.getUrl();
+        Throwable exception = context.getError();
+        RetStatus retStatus = RetStatus.RetSuccess;
+        int code = 0;
+        if (null != exception) {
+            retStatus = RetStatus.RetFail;
+            if (exception instanceof RpcException) {
+                RpcException rpcException = (RpcException) exception;
+                code = rpcException.getCode();
+                if (StringUtils.isNotBlank(rpcException.getMessage()) && rpcException.getMessage()
+                        .contains(PolarisBlockException.PREFIX)) {
+                    // 限流异常不进行熔断
+                    retStatus = RetStatus.RetFlowControl;
+                }
+                if (rpcException.isTimeout()) {
+                    retStatus = RetStatus.RetTimeout;
+                }
+            } else {
+                code = -1;
+            }
+        }
+
+        ServiceKey calleeServiceKey = createCalleeService(serviceInfo);
+        Resource resource = new InstanceResource(
+                calleeServiceKey,
+                url.getHost(),
+                url.getPort(),
+                new ServiceKey()
+        );
+        return new ResourceStat(resource, code, delay, retStatus);
+    }
+
+    private ServiceKey createCalleeService(DubboServiceInfo serviceInfo) {
+        return new ServiceKey(operator.getPolarisConfig().getNamespace(), serviceInfo.getService());
+    }
+
+    @Override
+    public int onSuccess(Object value) {
+        return 0;
+    }
+
+    @Override
+    public int onError(Throwable throwable) {
+        int code = 0;
+        if (throwable instanceof RpcException) {
+            RpcException rpcException = (RpcException) throwable;
+            code = rpcException.getCode();
+        } else {
+            code = -1;
+        }
+        return code;
+    }
+}
---
+++ dubbo-plugins/dubbo-registry-polaris/src/main/java/com/tencent/polaris/dubbo/report/ReportFilter.java
@@ -0,0 +1,117 @@
+/*
+ * Tencent is pleased to support the open source community by making dubbo-polaris-java available.
+ *
+ * Copyright (C) 2021 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.tencent.polaris.dubbo.report;
+
+
+import com.tencent.polaris.api.pojo.RetStatus;
+import com.tencent.polaris.api.utils.StringUtils;
+import com.tencent.polaris.circuitbreak.client.exception.CallAbortedException;
+import com.tencent.polaris.common.exception.PolarisBlockException;
+import com.tencent.polaris.common.registry.DubboServiceInfo;
+import com.tencent.polaris.common.registry.PolarisOperator;
+import com.tencent.polaris.common.registry.PolarisOperatorDelegate;
+import com.tencent.polaris.common.utils.DubboUtils;
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.constants.CommonConstants;
+import org.apache.dubbo.common.extension.Activate;
+import org.apache.dubbo.rpc.*;
+import org.apache.dubbo.rpc.model.ApplicationModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+@Activate(group = CommonConstants.CONSUMER, order = Integer.MIN_VALUE)
+public class ReportFilter extends PolarisOperatorDelegate implements Filter, Filter.Listener {
+
+    private static final String LABEL_START_TIME = "reporter_filter_start_time";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReportFilter.class);
+
+    private ApplicationModel applicationModel;
+
+    public ReportFilter() {
+        LOGGER.info("[POLARIS] init polaris reporter");
+    }
+
+    @Override
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        invocation.put(LABEL_START_TIME, System.currentTimeMillis());
+        return invoker.invoke(invocation);
+    }
+
+    @Override
+    public void onResponse(Result appResponse, Invoker<?> invoker, Invocation invocation) {
+        PolarisOperator polarisOperator = getGovernancePolarisOperator();
+        if (null == polarisOperator) {
+            return;
+        }
+        Long startTimeMilli = (Long) invocation.get(LABEL_START_TIME);
+        RetStatus retStatus = RetStatus.RetSuccess;
+        int code = 0;
+        if (appResponse.hasException()) {
+            retStatus = RetStatus.RetFail;
+            code = -1;
+        }
+        URL providerUrl = invoker.getUrl();
+        // 判断是否是应用级注册
+        long delay = System.currentTimeMillis() - startTimeMilli;
+        List<DubboServiceInfo> serviceInfos = DubboUtils.analyzeRemoteDubboServiceInfo(invoker, invocation);
+        DubboServiceInfo dubboServiceInfo = serviceInfos.get(0);
+        polarisOperator.reportInvokeResult(dubboServiceInfo.getService(), dubboServiceInfo.getReportMethodName(), providerUrl.getHost(),
+                providerUrl.getPort(), RpcContext.getServiceContext().getLocalHost(), delay, retStatus, code);
+    }
+
+    @Override
+    public void onError(Throwable t, Invoker<?> invoker, Invocation invocation) {
+        PolarisOperator polarisOperator = getGovernancePolarisOperator();
+        if (null == polarisOperator) {
+            return;
+        }
+        Long startTimeMilli = (Long) invocation.get(LABEL_START_TIME);
+        RetStatus retStatus = RetStatus.RetFail;
+        int code = -1;
+        if (t instanceof RpcException) {
+            RpcException rpcException = (RpcException) t;
+            code = rpcException.getCode();
+            if (isFlowControl(rpcException)) {
+                retStatus = RetStatus.RetFlowControl;
+            }
+            if (rpcException.isTimeout()) {
+                retStatus = RetStatus.RetTimeout;
+            }
+            if (rpcException.getCause() instanceof CallAbortedException) {
+                retStatus = RetStatus.RetReject;
+            }
+        }
+        URL url = invoker.getUrl();
+        long delay = System.currentTimeMillis() - startTimeMilli;
+        List<DubboServiceInfo> serviceInfos = DubboUtils.analyzeRemoteDubboServiceInfo(invoker, invocation);
+        DubboServiceInfo dubboServiceInfo = serviceInfos.get(0);
+        polarisOperator.reportInvokeResult(dubboServiceInfo.getService(), dubboServiceInfo.getReportMethodName(), url.getHost(),
+                url.getPort(), RpcContext.getServiceContext().getLocalHost(), delay, retStatus, code);
+    }
+
+    private boolean isFlowControl(RpcException rpcException) {
+        boolean a = StringUtils.isNotBlank(rpcException.getMessage()) && rpcException.getMessage()
+                .contains(PolarisBlockException.PREFIX);
+        boolean b = rpcException.isLimitExceed();
+        return a || b;
+    }
+
+}
