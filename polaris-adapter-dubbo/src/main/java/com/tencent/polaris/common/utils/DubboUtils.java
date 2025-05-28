8fad9e01daafc14366c18cf95ee0e38ec26d70ce
Haotian Zhang <928016560@qq.com>
2025-05-28 11:31:11 +0800

    fix:fix check error when register mode is null. (#79)

--- polaris-adapter-dubbo/src/main/java/com/tencent/polaris/common/utils/DubboUtils.java
+++ polaris-adapter-dubbo/src/main/java/com/tencent/polaris/common/utils/DubboUtils.java
@@ -17,6 +17,7 @@
 
 package com.tencent.polaris.common.utils;
 
+import com.tencent.polaris.api.utils.StringUtils;
 import com.tencent.polaris.common.registry.DubboServiceInfo;
 import org.apache.dubbo.common.URL;
 import org.apache.dubbo.common.config.ConfigurationUtils;
@@ -80,9 +81,13 @@ public class DubboUtils {
         List<DubboServiceInfo> serviceInfos = new ArrayList<>(2);
 
         URL providerUrl = invoker.getUrl();
+        String service = providerUrl.getRemoteApplication();
+        if (StringUtils.isBlank(service)) {
+            service = providerUrl.getHost();
+        }
         if (checkIsApplicationMode(invoker)) {
             serviceInfos.add(DubboServiceInfo.builder()
-                    .service(providerUrl.getRemoteApplication())
+                    .service(service)
                     .interfaceName(providerUrl.getServiceInterface())
                     .methodName(invocation.getMethodName())
                     .build());
@@ -95,14 +100,14 @@ public class DubboUtils {
         return serviceInfos;
     }
 
-    private static <T> boolean checkIsApplicationMode(Invoker<T> invoker) {
+    static <T> boolean checkIsApplicationMode(Invoker<T> invoker) {
         URL providerUrl = invoker.getUrl();
         if (providerUrl instanceof InstanceAddressURL) {
             return true;
         }
         if (providerUrl instanceof ServiceConfigURL) {
             ServiceConfigURL url = (ServiceConfigURL) providerUrl;
-            String registerMode = url.getParameter(RegistryConstants.REGISTER_MODE_KEY);
+            String registerMode = url.getParameter(RegistryConstants.REGISTER_MODE_KEY, RegistryConstants.DEFAULT_REGISTER_MODE_INSTANCE);
             switch (registerMode) {
                 case RegistryConstants.DEFAULT_REGISTER_MODE_ALL:
                 case RegistryConstants.DEFAULT_REGISTER_MODE_INSTANCE:
