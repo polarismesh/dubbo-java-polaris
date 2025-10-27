cf96cdab249a3883c21cc6b1e953a0e946d948ea
fishtailfu <fishtailfu@tencent.com>
2025-10-27 11:01:44 +0800

    feat: add spring boot example

---
+++ dubbo-examples/dubbo-springboot-example/dubbo-springboot-consumer/src/main/java/com/tencent/polaris/dubbo/example/springboot/consumer/GreetingServiceConsumer.java
@@ -0,0 +1,47 @@
+/*
+ * Tencent is pleased to support the open source community by making dubbo-polaris-java available.
+ *
+ * Copyright (C) 2021 Tencent. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.tencent.polaris.dubbo.example.springboot.consumer;
+
+import com.tencent.polaris.dubbo.example.api.EchoService;
+import com.tencent.polaris.dubbo.example.api.GreetingService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GreetingServiceConsumer {
+
+    @DubboReference(version = "1.0.0")
+//    @DubboReference
+    private GreetingService greetingService;
+
+    @DubboReference(version = "1.0.0")
+//    @DubboReference
+    private EchoService echoService;
+
+    public String doSayHello(String name) {
+        return greetingService.sayHello(name);
+    }
+
+    public String doSayHi(String name) {
+        return greetingService.sayHi(name);
+    }
+
+    public String doEcho(String value) {
+        return echoService.echo(value);
+    }
+}
---
+++ dubbo-examples/dubbo-springboot-example/dubbo-springboot-provider/src/main/java/com/tencent/polaris/dubbo/example/springboot/provider/GreetingServiceImpl.java
@@ -0,0 +1,38 @@
+/*
+ * Tencent is pleased to support the open source community by making dubbo-polaris-java available.
+ *
+ * Copyright (C) 2021 Tencent. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.tencent.polaris.dubbo.example.springboot.provider;
+
+import com.tencent.polaris.dubbo.example.api.GreetingService;
+import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.stereotype.Service;
+
+//@DubboService(version = "1.0.0")
+@DubboService
+@Service
+public class GreetingServiceImpl implements GreetingService {
+
+    @Override
+    public String sayHello(String name) {
+        return "[springboot] hello, " + name;
+    }
+
+    @Override
+    public String sayHi(String name) {
+        return "[provider by polaris] hi, " + name;
+    }
+}
