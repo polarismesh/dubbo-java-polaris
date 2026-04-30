63f05bcc301a6b715ca9bcf13ee47a644529eaf0
fishtailfu <fishtailfu@tencent.com>
2026-04-30 19:57:38 +0800

    feat: add spring cloud & dubbo example.

---
+++ dubbo-examples/dubbo-sct-example/dubbo-sct-consumer/src/main/java/com/tencent/polaris/dubbo/example/sct/consumer/HelloController.java
@@ -0,0 +1,36 @@
+/*
+ * Tencent is pleased to support the open source community by making dubbo-polaris-java available.
+ *
+ * Copyright (C) 2021 Tencent. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.tencent.polaris.dubbo.example.sct.consumer;
+
+import com.tencent.polaris.dubbo.example.api.GreetingService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @DubboReference
+    private GreetingService greetingService;
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(defaultValue = "world") String name) {
+        return greetingService.sayHello(name);
+    }
+}
